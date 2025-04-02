e963eb85a322ecefc96ce2641f5a90d3a6aa76dc
VolKuchyn <i.m.devil0906@gmail.com>
2025-04-02 20:33:26 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-vite-learning-process',
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  }
+  base: '/react-vite-learning-process'
 })
